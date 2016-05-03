c8006c194ab530a0ce75228e6b73e83f2e59ca27
j1wan <jiwanjeong@gmail.com>
2016-05-03 11:21:29 +0900

    first commit

---
+++ alltweets/constants.py
@@ -0,0 +1,13 @@
+USER_AGENT = 'EE'
+
+CONSUMER_KEY = '2F9ZOCModHxKmaBjjKRV6Uf1Z'
+CONSUMER_SECRET = 'UZztAzxAOFfZj8emU5p0Yew84Aay3qXPlnfVXNbBKPwWtqpHBp'
+
+ENDPOINTS = {
+    'statuses/user_timeline': ('GET', 'api'),
+    'search/tweets': ('GET', 'api'),
+    'followers/ids': ('GET',  'api'),
+    'followers/list': ('GET',  'api'),
+    'friends/ids': ('GET',  'api'),
+    'friends/list': ('GET',  'api')
+}
